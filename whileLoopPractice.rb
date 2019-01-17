71a1ea96309f7ca5e255e3502b5079335e595713
Matt Frazier <frazier3388@yahoo.com>
2019-01-17 14:14:27 +0900

    Practice file for while loops

---
+++ whileLoopPractice.rb
@@ -0,0 +1,11 @@
+first_name = ""
+user_input = nil
+while user_input != ""
+  puts "What is your first name?"
+  first_name = gets.chomp
+  puts "Nice to meet you #{first_name}!"
+  puts "What is your last name?"
+  last_name = gets.chomp
+  puts "Cool name #{first_name} #{last_name}!!"
+  puts "-" * 30
+end
