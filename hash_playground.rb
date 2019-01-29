831bd2b305220ba4e3bd6cc08e2b2c341eb64c22
Matt Frazier <frazier3388@yahoo.com>
2019-01-29 13:42:17 +0900

    Added comment and sample hash

--- hash_playground.rb
+++ hash_playground.rb
@@ -1,5 +1,13 @@
+
+# Hash a.k.a dictionary
+
 my_hash = { nameOne: "Matt", nameTwo: "Saki" }
+my_details = {'name' => 'Matt', 'favcolor' => 'blue'}
 
 my_hash.each do |k, v|
   puts "The key is #{k} and the value is #{v}!"
+end
+
+my_details.each do |k, v|
+  puts "Hello the key is #{k} and the value is #{v}"
 end
