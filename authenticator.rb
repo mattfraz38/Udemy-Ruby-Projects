0eb6ab67a4bbf0826cd6657ffdd5c7361768d0a8
Matt Frazier <frazier3388@yahoo.com>
2019-01-29 14:14:34 +0900

    added sample database and welcome directions

---
+++ authenticator.rb
@@ -0,0 +1,14 @@
+  users = [
+          { username: "mfraz",    password: "password1" },
+          { username: "kimfraz",  password: "password2" },
+          { username: "kelfraz",  password: "password3" },
+          { username: "jfraz",    password: "password4" },
+          { username: "silksaki", password: "password5" }
+        ]
+
+  puts "Welcome to the authenticator"
+  50.times { print "*"}
+  puts
+  puts "This is the program that will take authenticate the users username and password"
+  puts "If the password is correct, you will get back the user object"
+  
