f711d5eced23ea7d39a225e2d86b522332897938
Matt Frazier <frazier3388@yahoo.com>
2019-02-06 17:47:10 +0900

    created bcrypt practice file

---
+++ oop_projects/bcryptPractice.rb
@@ -0,0 +1,26 @@
+require 'bcrypt'
+
+my_password = BCrypt::Password.create("my password")
+#   #=> "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
+
+my_password1 = BCrypt::Password.create("my password")
+my_password2 = BCrypt::Password.create("my password")
+my_password3 = BCrypt::Password.create("my password")
+
+puts my_password
+puts my_password1
+puts my_password2
+puts my_password3
+
+puts my_password == "my password"
+puts my_password == my_password1
+
+# puts my_password.version              #=> "2a"
+# puts my_password.cost                 #=> 10
+# puts my_password == "my password"     #=> true
+# puts my_password == "not my password" #=> false
+
+# my_password = BCrypt::Password.new("$2a$10$G/cyhlb25eme8F/ChFaJju8yUOBO27fB2HkNVjzdxGJqtkY5LVPti")
+# # my_password == "my password"     #=> true
+# # my_password == "not my password" #=> false
+# puts my_password == "my password"
