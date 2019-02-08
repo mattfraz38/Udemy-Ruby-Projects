1969bcad86aac2ede590be22b7d67fb5aedc0966
Matt Frazier <frazier3388@yahoo.com>
2019-02-09 02:01:14 +0900

    verify_hash_digest working

--- oop_projects/practicalBcrypt.rb
+++ oop_projects/practicalBcrypt.rb
@@ -9,23 +9,26 @@ def verify_hash_digest(password)
   BCrypt::Password.new(password)
 end
 
-def authenticate_password(password)
-  verify_hash_digest(password)
-end
+# def authenticate_password(password)
+#   verify_hash_digest(password)
+# end
 
 loop do
   puts "Do you want to enter a password? y/n: "
   answer = gets.chomp
   if answer == "y"
     print "Enter a password: "
-    my_password = create_hash_digest(STDIN.noecho(&:gets).chomp)
+    puts my_password = create_hash_digest(STDIN.noecho(&:gets).chomp)
+    puts my_password = verify_hash_digest(my_password)
     puts
     puts my_password
     print "Enter your password again: "
-    verified_password = create_hash_digest(STDIN.noecho(&:gets).chomp)
+    puts verified_password = create_hash_digest(STDIN.noecho(&:gets).chomp)
+    puts verified_password = verify_hash_digest(verified_password)
     puts
     puts verified_password
-    if authenticate_password(my_password) == authenticate_password(verified_password)
+    if verified_password == my_password
+    # if authenticate_password(my_password) == authenticate_password(verified_password)
       puts
       puts "Yay! Your password matches!"
     else
